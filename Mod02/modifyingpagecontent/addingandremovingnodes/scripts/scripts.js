e20b029b76b3ead498ecafadf5de738d6e9acb85
Vanessa_Arce <vanessa.arce12@yahoo.com>
2019-08-07 22:12:11 -0400

    Added dropdown menu

--- Mod02/modifyingpagecontent/addingandremovingnodes/scripts/scripts.js
+++ Mod02/modifyingpagecontent/addingandremovingnodes/scripts/scripts.js
@@ -5,10 +5,26 @@ let imagesFiles = ["images/blouse.jpg", "images/boots.jpg", "images/dress.jpg", 
 let imageDescriptions = ["Blouse", "Boots", "Dress", "Jacket", "Pants", "Shirt", "Shoes", "Skirt"];
 
 window.onload = function () {
+    // Finds Id of clothes from the dropdownmenu
+    var select = document.getElementById("clothes");
+    //Loops thru the array to get array ImageDescriptions
+    for (let i = 0; i < imageDescriptions.length; i++) {
+        // Create element option for the dropdown values
+        let option = document.createElement("OPTION");
+        // Create text for the dropdown with values from the array
+        let txt = document.createTextNode(imageDescriptions[i]);
+        // Create new elemet 
+        option.appendChild(txt);
+        // Set attribute "value"
+        option.setAttribute("value", imageDescriptions[i]);
+        // Insert options from lastchild 
+        select.insertBefore(option, select.lastChild);
+    }
     // Finds imagesDiv
     let imageDiv = document.getElementById("imagesDiv")
     // Finds the add button
     let addBtn = document.getElementById("addBtn");
+    
     //When the user clicks the add button run this code
     addBtn.onclick = function () {
         // Finds the dropdown field    
@@ -31,8 +47,15 @@ window.onload = function () {
                 img.alt = imageDescriptions[i];
                 // Add the image in the browser to imageDiv 
                 imageDiv.appendChild(img)
+
             }
         }
+
     }
+    let resetBtn = document.getElementById("resetBtn");
+    resetBtn.onclick = function () {
+        document.getElementById("imagesDiv").innerHTML = "";
+    }
+
 }
 
